dc6d7fd794ea62d620da8fa982f7060e8eb75d12
pedrocarr <eng.pedro.amb@gmail.com>
2022-09-05 13:29:32 -0300

    implement post task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -12,5 +12,10 @@ export class TasksController {
   }
 
   @Post()
-  createTask();
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.createTask(title, description);
+  }
 }
