6137986eb9c7974efa32818c9f43199201963be0
pedrocarr <eng.pedro.amb@gmail.com>
2022-09-05 08:23:16 -0300

    generate controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TaskController } from './task/task.controller';
 
 @Module({
   imports: [TasksModule],
+  controllers: [TaskController],
 })
 export class AppModule {}
